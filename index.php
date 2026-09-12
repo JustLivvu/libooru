8f2d07cb72676af78584a408ffe9f12f4cd0401d
Liv <abithefemboy@proton.me>
2026-09-12 10:45:03 +0200

    removed slop

--- index.php
+++ index.php
@@ -221,10 +221,11 @@ function page_home(?array $user): void
     echo '  </form>';
     
     // Info Links
+    $postCount = (int)DB::scalar('SELECT COUNT(*) FROM posts');
     echo '  <div class="gelbooru-info-links">';
-    echo '    <span>Total visitors: ' . number_format($visitors) . '</span>';
-    echo '    &bull; ';
-    echo '    <span>Running ' . $e($siteName) . '</span>';
+    echo '    <span>Serving ' . number_format($postCount) . ' posts</span>';
+    echo '    - ';
+    echo '    <span>Running Libooru closed source software</span>';
     echo '  </div>';
     
     // Counter Container
